b64263327a922bc21a3678772fd9e84ecc2e5c4f
oksigenlee <fsyah1544@gmail.com>
2018-02-15 05:56:16 +0700

    studi kasus

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,9 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
+               
                 <div class="panel-heading">Data Siswa </div>
-                
+                <a href="{{ route('siswa.create') }}" class=" btn btn-sm btn-primary">Tambah</a>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -42,7 +43,7 @@
                                     <form action="{{ route('siswa.destroy', $datas->id) }}" method="post">
                                         {{ csrf_field() }}
                                         {{ method_field('DELETE') }}
-                                        <a href="{{ route('siswa.create') }}" class=" btn btn-sm btn-primary">Tambah</a>
+                                        
                                         <a href="{{ route('siswa.edit',$datas->id) }}" class=" btn btn-sm btn-primary">Edit</a>
                                         <button class="btn btn-sm btn-danger" type="submit" onclick="return confirm('Yakin ingin menghapus data?')">Delete</button>
                                         
